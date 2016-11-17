1c6da2b3b1ed3d37988943f7ac95cac7335cb430
Shane Slatter <shane.slatter95@gmail.com>
2016-11-16 20:44:16 -0500

    added comments

--- model/CPUList.py
+++ model/CPUList.py
@@ -1,19 +1,29 @@
 
-from urwid import Columns, AttrWrap, Text, ListBox, ExitMainLoop, MainLoop
+from urwid import Columns, Frame, Text, ListBox, ExitMainLoop, MainLoop
 from CPUMeter import CPUMeter
+from Footer import Footer
 from Palette import *
 
-class CPUList (ListBox):
+class CPUList(ListBox):
+	# Internals 
 	cpu_text = {}
 	cpu_columns = {}
-	cpu_meter = CPUMeter()
+	cpu_meter = None
 
-	def __init__ (self):
+	def __init__(self):
+		"""
+				Initializes the widgets
+		"""
+		self.cpu_meter = CPUMeter()
 		super(CPUList, self).__init__(self.cpu_columns)
 		self.update()
 
-	def update (self):
-		stat = [i.split() for i in [line.strip() for line in open('/proc/stat')]]
+	def update(self):
+		"""
+				Stores all CPU widgets
+				Updates child component
+		"""
+		stat = self.cpu_meter.readStat()
 
 		# Starts at 1 to skip first cpu
 		for i in range(1,len(stat)):
@@ -22,7 +32,7 @@ class CPUList (ListBox):
 
 		for i in range(len(self.cpu_text)):
 			self.cpu_columns[i] = Columns([
-				('fixed',  6, Text("%"+self.cpu_text[i+1], align='left')),
+				('fixed',  5, Text("%"+self.cpu_text[i+1], align='left')),
 				('fixed',  3, Text(" [", align='right')),
 				('weight', 1, self.cpu_meter[i]),
 				('fixed',  3, Text("] ", align='left')),
@@ -32,8 +42,8 @@ class CPUList (ListBox):
 
 # Testing
 if __name__ == '__main__':
-
 	lb = CPUList()
+	frame = Frame(lb, header=None, footer=Footer())
 
 	def exit(key):
 		if key in ('q', 'Q'):
@@ -43,6 +53,6 @@ if __name__ == '__main__':
 		lb.update()
 		loop.set_alarm_in(1, refresh)
 
-	main_loop = MainLoop(lb, palette, unhandled_input=exit)
+	main_loop = MainLoop(frame, palette, unhandled_input=exit)
 	main_loop.set_alarm_in(1, refresh)
 	main_loop.run()
--- model/CPUMeter.py
+++ model/CPUMeter.py
@@ -1,39 +1,53 @@
 
 import time
-from urwid import ProgressBar, AttrWrap, ExitMainLoop, MainLoop, ListBox, SimpleListWalker
+from urwid import Frame, ProgressBar, AttrMap, ExitMainLoop, MainLoop, ListBox, SimpleListWalker
+from Footer import Footer
 from Palette import *
 
 class CPUMeter(SimpleListWalker):
+	# Internals
 	cpu_meter = {}
 
 	def __init__(self):
+		"""
+				Initializes the widget
+		"""
 		super(CPUMeter, self).__init__(self)
 		self.update()
 
 	def update(self):
-		c1 = [i.split() for i in [line.strip() for line in open('/proc/stat')]]
+		"""
+				Calculates cpu percentage
+		"""
+		c1 = self.readStat()
 		time.sleep(0.5)
-		c2 = [i.split() for i in [line.strip() for line in open('/proc/stat')]]
+		c2 = self.readStat()
 
 		# Starts at 1 to skip first cpu
 		for i in range(1,len(c1)):
 			if 'cpu' in c1[i][0]:
-				top = int(c1[i][1])+int(c1[i][2])+int(c1[i][3])- \
-							int(c2[i][1])-int(c2[i][2])-int(c2[i][3])
-				btm = int(c1[i][1])+int(c1[i][2])+int(c1[i][3])+int(c1[i][4])- \
-							int(c2[i][1])-int(c2[i][2])-int(c2[i][3])-int(c2[i][4])*1.0
+				perc = (int(c1[i][1])+int(c1[i][2])+int(c1[i][3])-                     \
+							  int(c2[i][1])-int(c2[i][2])-int(c2[i][3]))/                    \
+				       (int(c1[i][1])+int(c1[i][2])+int(c1[i][3])+int(c1[i][4])-       \
+							  int(c2[i][1])-int(c2[i][2])-int(c2[i][3])-int(c2[i][4])*1.0)*100
 				if i in self.cpu_meter:
-					self.cpu_meter[i] = (top/btm*100)
+					self.cpu_meter[i] = perc
 					self[i-1].set_completion(self.cpu_meter[i])
 				else:
-					self.cpu_meter[i] = (top/btm*100)
-					self.append(ProgressBar('body', 'progress', self.cpu_meter[i], 100))	
+					self.cpu_meter[i] = perc
+					self.append(ProgressBar('body', 'progress', self.cpu_meter[i], 100))  
+
+	def readStat(self):
+		"""
+				Returns list of CPU info
+		"""
+		return [i.split() for i in [line.strip() for line in open('/proc/stat')]]
+
 
 # Testing
 if __name__ == '__main__':
-	
 	cm = CPUMeter()
-	lb = ListBox(cm)
+	frame = Frame(ListBox(cm), header=None, footer=Footer())
 
 	def exit(key):
 		if key in ('q', 'Q'):
@@ -43,6 +57,6 @@ if __name__ == '__main__':
 		cm.update()
 		loop.set_alarm_in(0.5, refresh)
 
-	main_loop = MainLoop(lb, palette, unhandled_input=exit)
+	main_loop = MainLoop(frame, palette, unhandled_input=exit)
 	main_loop.set_alarm_in(0.5, refresh)
 	main_loop.run()
